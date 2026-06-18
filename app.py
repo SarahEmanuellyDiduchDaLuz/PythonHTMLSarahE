33072c0b6ffe26c19a698cd96bfc49f76826841f
SarahEmanuellyDiduchDaLuz <diduch.luz@escola.pr.gov.br>
2026-06-18 12:44:18 +0000

    ok

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ def início():
     curso = "Python com HTML"
 
     return render_template(
-        'index.html'
+        'index.html',
         nome = nome,
         curso = curso
     )
+
+@app.route('/sobre')
+def sobre():
+    return"""
+    <h1>SObre o Projeto</h1>
+    <p>Esse projeto foi crioado usando Python e Flask.</p>
+    <a href="/"> Volta para inicio<a>
+    """
+if __name__ == '__main__'
+    app.run(host='0.0.0.0', port=3000, debug=True)
